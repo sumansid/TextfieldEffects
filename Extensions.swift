49566fa8e6b35c97d13aebc7731684a4d515d89f
niv ben-porath <nbpapps@gmail.com>
2020-04-20 00:15:39 +0300

    added method and some changes
    1) In the "setIcon" I chnaged the width of "iconContainerView" to give some space between icon and corser
    
    2)In "addShadowToTextField" I removed setting the backgroundColor to White, so that the user can set the color
    
    3) Added a "setTextAlignment" to set some test attributes

--- Extensions.swift
+++ Extensions.swift
@@ -17,7 +17,7 @@ extension UITextField {
         iconView.tintColor = .blue
         iconView.image = image
         let iconContainerView: UIView = UIView(frame:
-            CGRect(x: 20, y: 0, width: 30, height: 30))
+            CGRect(x: 20, y: 0, width: 40, height: 30)) // chnaged width from 30 to 40 to give some space between icon and corser
         iconContainerView.addSubview(iconView)
         leftView = iconContainerView
         leftViewMode = .always
@@ -25,32 +25,40 @@ extension UITextField {
     }
     
     func addShadowToTextField(color: UIColor = UIColor.gray, cornerRadius: CGFloat) {
-
-     self.backgroundColor = UIColor.white
-     self.layer.masksToBounds = false
-     self.layer.shadowColor = color.cgColor
-     self.layer.shadowOffset = CGSize(width: 0, height: 0)
-     self.layer.shadowOpacity = 1.0
-     self.backgroundColor = .white
-     self.layer.cornerRadius = cornerRadius
+        //removed setting the background color so it can be set by the user
+        //     self.backgroundColor = UIColor.white
+        self.backgroundColor = UIColor.white
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowOpacity = 1.0
+        //     self.backgroundColor = .white
+        self.layer.cornerRadius = cornerRadius
         
     }
     
-    func setBottomBorder() {
-      self.borderStyle = .none
-      self.layer.backgroundColor = UIColor.white.cgColor
-      self.layer.masksToBounds = false
-      self.layer.shadowColor = UIColor.gray.cgColor
-      self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
-      self.layer.shadowOpacity = 1.0
-      self.layer.shadowRadius = 0.0
+    func setBottomBorder(color : UIColor = UIColor.white, shadowColor: UIColor = UIColor.gray) {
+        self.borderStyle = .none
+        self.layer.backgroundColor = color.cgColor
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = shadowColor.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
         
     }
     
     func changePlaceholderColor(placeholder : String, color: UIColor) {
         self.attributedPlaceholder = NSAttributedString(string: placeholder,
-        attributes: [NSAttributedString.Key.foregroundColor: color])
-
+                                                        attributes: [NSAttributedString.Key.foregroundColor: color])
+        
+    }
+    
+    //Added this to setup the text attributes
+    func setTextAlignment(textAlignment : NSTextAlignment = .left, fontSize : CGFloat = 14.0,weight : UIFont.Weight = .regular) {
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: weight)
+        
     }
     
     
