3019428e12edd64792c9c365e12cce8b9cbe6783
sumansid <53033648+sumansid@users.noreply.github.com>
2020-04-19 15:47:17 -0400

    Add ShadowColor argument

--- Extensions.swift
+++ Extensions.swift
@@ -34,11 +34,11 @@ extension UITextField {
         
     }
     
-    func setBottomBorder(color : UIColor = UIColor.white) {
+    func setBottomBorder(color : UIColor = UIColor.white, shadowColor: UIColor = UIColor.gray) {
       self.borderStyle = .none
       self.layer.backgroundColor = color.cgColor
       self.layer.masksToBounds = false
-      self.layer.shadowColor = UIColor.gray.cgColor
+      self.layer.shadowColor = shadowCo.cgColor
       self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
       self.layer.shadowOpacity = 1.0
       self.layer.shadowRadius = 0.0
